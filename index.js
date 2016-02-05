cf2bb5ee3f11c5f47f5ec075e066dac00edc4468
Alyson <emerald.aly@gmail.com>
2016-02-05 18:57:12 -0500

    Upload initial files and image.js

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var BodyParser = require("body-parser");
+var app = express();
+var PORT = 8080;
+
+app.use(BodyParser.urlencoded({extended: false}));
+
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/home.html");
+
+});
+
+app.get("/AboutMe", function(req, res) {
+  res.sendFile(process.cwd() + "/views/AboutMe.html")
+});
+
+app.get("/blog", function(req, res  {
+  res.sendFile(process,cwd() } "/views/blog.html");
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on port %s", PORT);
+});
