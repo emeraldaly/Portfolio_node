dcf62d296425089e7fe381e8c258632030c57d8f
Alyson <emerald.aly@gmail.com>
2016-02-09 14:19:21 -0500

    added new background

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require("express");
 var BodyParser = require("body-parser");
+var GitHubApi = require('node-github');
+
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
+
+app.use("/js", express.static("public/js"));
+app.use("/css", express.static("public/css"));
 
 app.use(BodyParser.urlencoded({extended: false}));
 
@@ -16,6 +21,23 @@ app.get("/AboutMe", function(req, res) {
 
 app.get("/blog", function(req, res)  {
   res.sendFile(process,cwd() + "/views/blog.html");
+});
+
+app.get("/projects", function(req, res)  {
+  res.sendFile(process,cwd() + "/views/projects.html");
+});
+
+app.get('/dashboard/:githubname', function(req, res) {
+  var github = new GitHubApi({
+    version: "3.0.0"
+    });
+  console.log(req.params.githubname)
+github.user.getFrom({
+      user: req.params.githubname
+  }, function(err, gitResponse){
+    // console.log(gitResponse);
+      res.send(JSON.stringify(gitResponse))
+  });
 });
 
 app.listen(PORT, function() {
